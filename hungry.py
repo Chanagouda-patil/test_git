dc608f582f620c85015f965d9ec5f3e44ce9f961
Chanagouda-patil <patilchanagouda1995@gmail.com>
2022-09-05 12:30:58 +0530

    New branch

--- hungry.py
+++ hungry.py
@@ -2,4 +2,4 @@ print("I'm not hungry, i Just ate some snacks")
 print("I ate Pizza")
 thirsty = input("Are you thirsty?")
 if thirsty == "Yes":
-    print("Drink Water")
+    print("Drink Cool-drinks")
